1e971b418e0a013f2a9bc6374ca335e3e27af522
Gene <g.larose@frontier.com>
2018-11-19 01:46:31 -0500

    initial commit
    initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\owner\desktop\desktop\udemy\courses\django_python_realtor_app\project\btre_project\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
